a0cffc22490458c349c98f1b49c1b77f06f9e7dd
Vaibhav Madhavan <vaibhav.madhavan14@gmail.com>
2026-02-21 22:58:04 +0400

    Remove node_modules and .next from tracking, add .gitignore

--- .next/dev/server/chunks/ssr/src_app_layout_50409fe5.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/src/app/layout.js [app-rsc] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=src_app_layout_50409fe5.js.map
